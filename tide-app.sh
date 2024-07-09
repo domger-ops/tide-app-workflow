ba0b2e6749622af858e9e3c1825b0ebb532fa483
domger-ops <senddommail@gmail.com>
2024-07-08 22:42:55 -0700

    add high tide data points and push metrics

--- tide-app.sh
+++ tide-app.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Pushgateway URL
 pushgateway_url="http://pushgateway.monitoring.svc.cluster.local:9091/metrics/job/tide_app"
 
-# Function to fetch and display tide and weather details for a given location using the Stormglass API
 fetch_and_display_tide_and_weather() {
   local park_name="$1"
   local city="$2"
@@ -11,71 +9,64 @@ fetch_and_display_tide_and_weather() {
   local lat="$4"
   local lon="$5"
   local station="$6"
-  local fallback_lat="${7:-}" # Fallback latitude, optional
-  local fallback_lon="${8:-}" # Fallback longitude, optional
+  local fallback_lat="${7:-}"
+  local fallback_lon="${8:-}"
 
-  # Current date in YYYYMMDD format
-  local date=$(date -u +"%Y-%m-%d") # Format: YYYY-MM-DD
-  local today=$(date -u +"%Y%m%d") # Format: YYYYMMDD, used for API calls
+  local date=$(date -u +"%Y-%m-%d")
+  local today=$(date -u +"%Y%m%d")
 
-  # Display location details
-  echo -e "Fetching lowest tide details for $date:\n$park_name"
+  echo -e "Fetching tide details for:\n$park_name - $date"
 
-  # Modify NOAA API URL to use the station variable
   local noaa_tide_api_url="https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=${today}&range=24&station=${station}&product=predictions&datum=MLLW&time_zone=lst_ldt&interval=hilo&units=english&application=TidePool&format=json"
-
-  # Fetch tide data using the NOAA API
   local tide_data=$(curl -s "${noaa_tide_api_url}")
 
-  # Extract tide information from the JSON response
-  # Value: Lowest Tide Time (HH:MM) - eliminates the date, allowing for easier use of value when needed for weather api parsing
   local lowest_tide_time=$(echo "$tide_data" | jq -r '.predictions | sort_by(.v | tonumber) | .[0].t' | cut -d' ' -f2)
-
-  # Value: Lowest Tide Number
+  local highest_tide_time=$(echo "$tide_data" | jq -r '.predictions | sort_by(.v | tonumber) | reverse | .[0].t' | cut -d' ' -f2)
   local lowest_tide_value=$(echo "$tide_data" | jq -r '.predictions | sort_by(.v | tonumber) | .[0].v')
-
-  # Assuming lowest_tide_time is in the format "HH:MM"
-  lowest_tide_hour=$(echo "$lowest_tide_time" | cut -d':' -f1) # Extracts the hour part
-  lowest_tide_time_number=$((10#$lowest_tide_hour)) # Converts the hour to a number to use for weather API
-
-  # Display tide details
+  local highest_tide_value=$(echo "$tide_data" | jq -r '.predictions | sort_by(.v | tonumber) | reverse | .[0].v')
+  lowest_tide_hour=$(echo "$lowest_tide_time" | cut -d':' -f1)
+  lowest_tide_time_number=$((10#$lowest_tide_hour))
+  highest_tide_hour=$(echo "$highest_tide_time" | cut -d':' -f1)
+  highest_tide_time_number=$((10#$highest_tide_hour))
+  
   echo "Lowest tide time: $lowest_tide_time, Tide number: $lowest_tide_value"
+  echo "Highest tide time: $highest_tide_time, Tide number: $highest_tide_value"
 
-  # Fetch and display weather details using the National Weather Service API
-  fetch_weather_data() {
-    local lat="$1"
-    local lon="$2"
-    local urls=$(curl -s "https://api.weather.gov/points/${lat},${lon}" | jq -r '.properties.forecastHourly')
-    local weather_data=$(curl -s "$urls")
-    local temp=$(echo "$weather_data" | jq -r --arg index "$lowest_tide_time_number" '.properties.periods[$index|tonumber].temperature')
-    echo "$temp"
-  }
-
-  local temp=$(fetch_weather_data "$lat" "$lon")
-  if [ "$temp" = "null" ] || [ -z "$temp" ] && [ -n "$fallback_lat" ] && [ -n "$fallback_lon" ]; then
-    echo "Primary location failed, trying nearby location..."
-    temp=$(fetch_weather_data "$fallback_lat" "$fallback_lon")
-  fi
-
-  if [ "$temp" = "null" ] || [ -z "$temp" ]; then
-    echo "Temperature data not available."
-  else
-    echo "Temperature: ${temp}°F"
-  fi
-  echo
-
-  # Push metrics to Pushgateway
-  echo "Pushing metrics to Pushgateway..."
-  curl -X POST --data-binary @- "${pushgateway_url}" <<EOF
-tide_lowest_value{park_name="$park_name", city="$city", state="$state"} $lowest_tide_value
-tide_lowest_time{park_name="$park_name", city="$city", state="$state"} $lowest_tide_time
-weather_temperature{park_name="$park_name", city="$city", state="$state"} $temp
-EOF
-
-  echo "Metrics pushed successfully."
+  fetch_weather_data "$lat" "$lon" "$lowest_tide_time_number" "$highest_tide_time_number" "$fallback_lat" "$fallback_lon"
 }
 
-# Array of locations
+fetch_weather_data() {
+    local lat="$1"
+    local lon="$2"
+    local lowest_tide_time_number="$3"  
+    local highest_tide_time_number="$4"
+    local fallback_lat="$5"
+    local fallback_lon="$6"
+    local urls=$(curl -s "https://api.weather.gov/points/${lat},${lon}" | jq -r '.properties.forecastHourly')
+    local weather_data=$(curl -s "$urls")
+
+    local temp_low=$(echo "$weather_data" | jq -r --arg index "$lowest_tide_time_number" '.properties.periods[$index|tonumber].temperature')
+    local temp_high=$(echo "$weather_data" | jq -r --arg index "$highest_tide_time_number" '.properties.periods[$index|tonumber].temperature')
+
+    if [[ "$temp_low" == "null" || -z "$temp_low" || "$temp_high" == "null" || -z "$temp_high" ]]; then
+      if [[ -n "$fallback_lat" && -n "$fallback_lon" ]]; then
+        echo "Primary location failed, trying nearby location..."
+        urls=$(curl -s "https://api.weather.gov/points/${fallback_lat},${fallback_lon}" | jq -r '.properties.forecastHourly')
+        weather_data=$(curl -s "$urls")
+        temp_low=$(echo "$weather_data" | jq -r --arg index "$lowest_tide_time_number" '.properties.periods[$index|tonumber].temperature')
+        temp_high=$(echo "$weather_data" | jq -r --arg index "$highest_tide_time_number" '.properties.periods[$index|tonumber].temperature')
+      fi
+    fi
+
+    if [[ "$temp_low" == "null" || -z "$temp_low" || "$temp_high" == "null" || -z "$temp_high" ]]; then
+      echo "Temperature data not available."
+    else
+      echo "Temperature at low tide: ${temp_low}°F" 
+      echo "Temperature at high tide: ${temp_high}°F"
+    fi
+    echo
+}
+
 locations=(
   "Point Loma Tide Pools,San Diego,CA,32.6731,-117.2425,9410170,32.7157,-117.1611"
   "Crystal Cove State Park,Laguna Beach,CA,33.5665,-117.8090,9410580,33.5427,-117.7854"
@@ -87,7 +78,6 @@ locations=(
   "Shi Shi Beach Tide Pools,Neah Bay,WA,48.2705,-124.6884,9443090,48.3687,-124.6252"
 )
 
-# Loop through each location and fetch/display tide and weather details
 for location in "${locations[@]}"; do
   IFS=',' read -r park_name city state lat lon station fallback_lat fallback_lon <<< "$location"
   fetch_and_display_tide_and_weather "$park_name" "$city" "$state" "$lat" "$lon" "$station" "$fallback_lat" "$fallback_lon"
